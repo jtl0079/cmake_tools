9210001cb4fa709f25ab25cde054666dc4df5f1b
name_lss01 <you@example.com>
2026-05-31 15:03:16 +0800

    12

--- cmake/core/dependency/std/pipeline/ffmpeg/core_dependency_std_pipeline_download_and_unzip_ffmpeg_prebuilt_and_create_ffmpeg_target.cmake
+++ cmake/core/dependency/std/pipeline/ffmpeg/core_dependency_std_pipeline_download_and_unzip_ffmpeg_prebuilt_and_create_ffmpeg_target.cmake
@@ -27,6 +27,11 @@
 # TARGET_NAME = "FFmpeg"
 
 
+get_filename_component(_current_dir ${CMAKE_CURRENT_LIST_FILE} PATH)
+include(${_current_dir}/../../backend/ffmpeg/fetch/core_dependency_std_backend_download_ffmpeg_prebuilt.cmake)
+include(${_current_dir}/../../backend/ffmpeg/create/core_dependency_std_backend_create_ffmpeg_target.cmake)
+
+
 function(core_dependency_std_pipeline_download_and_unzip_ffmpeg_prebuilt_and_create_ffmpeg_target)
     set(options)
     set(one_value_args  FILENAME 
@@ -37,6 +42,7 @@ function(core_dependency_std_pipeline_download_and_unzip_ffmpeg_prebuilt_and_cre
     set(multi_value_args "")
     cmake_parse_arguments(PIPELINE "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
     
+
     # ====================================
     #    convert boolean variables
     # ====================================
