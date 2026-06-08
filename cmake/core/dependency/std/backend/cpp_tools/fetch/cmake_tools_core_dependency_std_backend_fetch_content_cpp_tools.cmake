0c3cfe1a47d397fd42e770fea11ff09208d46391
name_lss01 <you@example.com>
2026-06-08 20:11:55 +0800

    fef

--- cmake/core/dependency/std/backend/cpp_tools/fetch/cmake_tools_core_dependency_std_backend_fetch_content_cpp_tools.cmake
+++ cmake/core/dependency/std/backend/cpp_tools/fetch/cmake_tools_core_dependency_std_backend_fetch_content_cpp_tools.cmake
@@ -1,14 +1,9 @@
-# ====== core_dependency_std_backend_fetch_content_cpp_tools.cmake
+# ====== cmake_tools_core_dependency_std_backend_fetch_content_cpp_tools.cmake
 # ====================================
 #		explanation
 # ====================================
 # Download and import cpp_tools repository by using FetchContent.
-#
-# Repository:
-# https://github.com/jtl0079/cpp_tools.git
-#
 # The target project will be added through FetchContent_MakeAvailable().
-#
 
 # ====================================
 #		parameters
@@ -19,29 +14,42 @@
 # ====================================
 #		parameter default value
 # ====================================
-# GIT_TAG = main
-# IS_SILENT_MODE = OFF
+# GIT_TAG        = main
+# IS_SILENT_MODE = FALSE
 
 # ====================================
 #       return variables
 # ====================================
-# RETURN_VAR_PREFIX = CORE_DEPENDENCY_STD_BACKEND_FETCH_CONTENT_CPP_TOOLS
-# ${RETURN_VAR_PREFIX}_REPOSITORY_URL
+# RETURN_VAR_PREFIX = CMAKE_TOOLS_CORE_DEPENDENCY_STD_BACKEND_FETCH_CONTENT_CPP_TOOLS
+# ${RETURN_VAR_PREFIX}_REPOSITORY_URL = "https://github.com/jtl0079/cpp_tools.git"
 # ${RETURN_VAR_PREFIX}_GIT_TAG
 # ${RETURN_VAR_PREFIX}_SOURCE_DIR
 # ${RETURN_VAR_PREFIX}_BINARY_DIR
 
+function(cmake_tools_core_dependency_std_backend_fetch_content_cpp_tools)
 
-function(core_dependency_std_backend_fetch_content_cpp_tools)
+	# ====================================
+	#		pre-variables
+	# ====================================
+	set(this_function_name  "CMAKE_TOOLS_CORE_DEPENDENCY_STD_BACKEND_FETCH_CONTENT_CPP_TOOLS" )
+	
+
+	# ====================================
+	#		includes
+	# ====================================
+	include(FetchContent)
+
 
 	# ====================================
 	#		parameters
 	# ====================================
 	set(options)
+
 	set(oneValueArgs
 		GIT_TAG
 		IS_SILENT_MODE
 	)
+
 	set(multiValueArgs)
 
 	cmake_parse_arguments(
@@ -52,21 +60,21 @@ function(core_dependency_std_backend_fetch_content_cpp_tools)
 		${ARGV}
 	)
 
+
 	# ====================================
 	#		parameter default value
 	# ====================================
 	if(NOT DEFINED ARG_GIT_TAG)
-		set(ARG_GIT_TAG "main")
+		set(ARG_GIT_TAG main)
 	endif()
 
 	if(NOT DEFINED ARG_IS_SILENT_MODE)
-		set(ARG_IS_SILENT_MODE OFF)
+		set(ARG_IS_SILENT_MODE FALSE)
 	endif()
 
-	include(FetchContent)
 
 	# ====================================
-	#       dependency
+	#		Logic
 	# ====================================
 	FetchContent_Declare(
 		cpp_tools
@@ -74,49 +82,26 @@ function(core_dependency_std_backend_fetch_content_cpp_tools)
 		GIT_TAG        ${ARG_GIT_TAG}
 	)
 
+	FetchContent_MakeAvailable(cpp_tools)
+
 	FetchContent_GetProperties(cpp_tools)
 
-	if(NOT cpp_tools_POPULATED)
-		FetchContent_MakeAvailable(cpp_tools)
-	endif()
-
-	# ====================================
-	#       return variables
-	# ====================================
-	set(RETURN_VAR_PREFIX  CORE_DEPENDENCY_STD_BACKEND_FETCH_CONTENT_CPP_TOOLS  )
-
-	set(${RETURN_VAR_PREFIX}_REPOSITORY_URL "https://github.com/jtl0079/cpp_tools.git" )
-	set(${RETURN_VAR_PREFIX}_GIT_TAG "${ARG_GIT_TAG}" )
-	set(${RETURN_VAR_PREFIX}_SOURCE_DIR "${cpp_tools_SOURCE_DIR}" )
-	set(${RETURN_VAR_PREFIX}_BINARY_DIR "${cpp_tools_BINARY_DIR}" )
-
-	set(${RETURN_VAR_PREFIX}_REPOSITORY_URL
-		"https://github.com/jtl0079/cpp_tools.git"
-		PARENT_SCOPE
-	)
-
-	set(${RETURN_VAR_PREFIX}_GIT_TAG
-		"${ARG_GIT_TAG}"
-		PARENT_SCOPE
-	)
-
-	set(${RETURN_VAR_PREFIX}_SOURCE_DIR
-		"${cpp_tools_SOURCE_DIR}"
-		PARENT_SCOPE
-	)
-
-	set(${RETURN_VAR_PREFIX}_BINARY_DIR
-		"${cpp_tools_BINARY_DIR}"
-		PARENT_SCOPE
-	)
 
 	# ====================================
 	#       print return variables
 	# ====================================
 	if(NOT ARG_IS_SILENT_MODE)
 
+		set(RETURN_VAR_PREFIX ${this_function_name})
+
 		message(STATUS "")
 		message(STATUS "[${RETURN_VAR_PREFIX} - print return variables]")
+
+		# Var for print
+		set(${RETURN_VAR_PREFIX}_REPOSITORY_URL "https://github.com/jtl0079/cpp_tools.git")
+		set(${RETURN_VAR_PREFIX}_GIT_TAG "${ARG_GIT_TAG}")
+		set(${RETURN_VAR_PREFIX}_SOURCE_DIR "${cpp_tools_SOURCE_DIR}")
+		set(${RETURN_VAR_PREFIX}_BINARY_DIR "${cpp_tools_BINARY_DIR}")
 
 		foreach(temp_print_return_var IN ITEMS
 			"${RETURN_VAR_PREFIX}_REPOSITORY_URL"
@@ -131,5 +116,35 @@ function(core_dependency_std_backend_fetch_content_cpp_tools)
 		endforeach()
 
 	endif()
+
+
+	# ====================================
+	#       return variables
+	# ====================================
+	set(RETURN_VAR_PREFIX ${this_function_name})
+
+	set(
+		${RETURN_VAR_PREFIX}_REPOSITORY_URL
+		"https://github.com/jtl0079/cpp_tools.git"
+		PARENT_SCOPE
+	)
+
+	set(
+		${RETURN_VAR_PREFIX}_GIT_TAG
+		"${ARG_GIT_TAG}"
+		PARENT_SCOPE
+	)
+
+	set(
+		${RETURN_VAR_PREFIX}_SOURCE_DIR
+		"${cpp_tools_SOURCE_DIR}"
+		PARENT_SCOPE
+	)
+
+	set(
+		${RETURN_VAR_PREFIX}_BINARY_DIR
+		"${cpp_tools_BINARY_DIR}"
+		PARENT_SCOPE
+	)
 
 endfunction()
--- docs/raw_eng/rules of cmake content.md
+++ docs/raw_eng/rules of cmake content.md
@@ -1,3 +1,4 @@
+# Rules of .cmake files content
 A .cmake files should record the info below as the comment at top:
 ```
 # ====== ${file_name}.cmake
@@ -9,47 +10,60 @@ A .cmake files should record the info below as the comment at top:
 # ====================================
 #		parameters
 # ====================================
-# ${param_name} : explanation
+# ${param_name}  : explanation
+# IS_SILENT_MODE : 
 
 # ====================================
 #		parameter default value
 # ====================================
-# ${param_name} = default value 
-
+# ${param_name}  = default value 
+# IS_SILENT_MODE = FALSE 
 
 # ====================================
 #       return variables
 # ====================================
-# RETURN_VAR_PREFIX = ${function_name}
-# ${RETURN_VAR_PREFIX}_VAR1 = value
-# ${RETURN_VAR_PREFIX}_VAR2 = value
+# return_var_prefix = ${function_name}
+# ${return_var_prefix}_VAR1 = value
+# ${return_var_prefix}_VAR2 = value
 
 ```
 
 Structure within a function:
 ```
 function()
-	
+	# ====================================
+	#		pre-variables
+	# ====================================
+	set(this_function_name "${SCREAMING_SNAKE_CASE_FUNCTION_NAME}")
+	set(return_var_prefix ${this_function_name})
+
+
+	# ====================================
+	#		includes
+	# ====================================
+	include()
+
+
+	# ====================================
+	#       function start prompt
+	# ====================================
+	if(NOT IS_SILENT_MODE)
+		message(STATUS "")
+		message(STATUS "[${this_function_name} - start]")
+	endif()
+
+
 	# ====================================
 	#		parameters
 	# ====================================
+	set(options)
+	set(oneValueArgs  IS_SILENT_MODE)
+	set(multiValueArgs)
 
 
 	# ====================================
 	#		parameter default value
 	# ====================================
-
-
-	# ====================================
-	#       return variables
-	# ====================================
-	set(RETURN_VAR_PREFIX ${function_name})
-
-	set(${RETURN_VAR_PREFIX}_VAR1 "value 1")	# For print
-	set(${RETURN_VAR_PREFIX}_VAR2 "value 2")
-
-	set(${RETURN_VAR_PREFIX}_VAR1 "value 1" PARENT_SCOPE)	# For return 
-	set(${RETURN_VAR_PREFIX}_VAR2 "value 2" PARENT_SCOPE)
 
 
 	# ====================================
@@ -59,6 +73,10 @@ function()
 		message(STATUS "")
 		message(STATUS "[${RETURN_VAR_PREFIX} - print return variables]")
 
+		# Var for print
+		set(${RETURN_VAR_PREFIX}_VAR1 "value 1")	
+		set(${RETURN_VAR_PREFIX}_VAR2 "value 2")
+
 		foreach(temp_print_return_var IN ITEMS
 			"${RETURN_VAR_PREFIX}_VAR1"
 			"${RETURN_VAR_PREFIX}_VAR2"
@@ -66,6 +84,13 @@ function()
 			message(STATUS "${temp_print_return_var} = ${${temp_print_return_var}}")
 		endforeach()
 	}
+
+
+	# ====================================
+	#       return variables
+	# ====================================
+	set(${return_var_prefix}_VAR1 "value 1" PARENT_SCOPE)	# For return 
+	set(${return_var_prefix}_VAR2 "value 2" PARENT_SCOPE)
 
 	
 endfunction()
